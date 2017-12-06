f10537af6a85d94c7fb49b500002549a2f69e3d3
NeoYin <yinwei6@jd.com>
2017-12-06 14:23:23 +0800

    init project

---
+++ src/main/java/me/keepup/neo/keepupwx/KeepupWxApplication.java
@@ -0,0 +1,12 @@
+package me.keepup.neo.keepupwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepupWxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepupWxApplication.class, args);
+	}
+}
---
+++ src/test/java/me/keepup/neo/keepupwx/KeepupWxApplicationTests.java
@@ -0,0 +1,16 @@
+package me.keepup.neo.keepupwx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeepupWxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
