534ab63b30eae08cf7c73bebb3fc224fd592d2e3
NeoYin <yinwei6@jd.com>
2018-01-22 18:17:12 +0800

    add config impl

--- src/main/java/me/keepup/neo/wx/utils/WeixinPayUtil.java
+++ src/main/java/me/keepup/neo/wx/utils/WeixinPayUtil.java
@@ -2,20 +2,30 @@ package me.keepup.neo.wx.utils;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component("weixinPayUtil")
 public class WeixinPayUtil {
 
 
-    /**
-     * appid： wxd930ea5d5a258f4f
-       mch_id： 商户Id
-       body：商品描述
-       nonce_str：随机字符串
-     * @param appId
-     * @return
-     */
-    public static String generatePaySign(String appId,String nonceStr){
-        return null;
-    }
+
+  public Map<String, String> perPay(String body) throws Exception {
+
+      Map<String, String> map = new HashMap<>();
+      //商品简单描述
+      map.put("body",body);
+      //商户系统内部订单号，要求32个字符内，只能是数字、大小写字母_-|*@ ，且在同一个商户号下唯一。
+      map.put("out_trade_no","");
+      //订单总金额，单位为分
+      map.put("total_fee","");
+        //APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP。
+      map.put("spbill_create_ip","");
+      //异步接收微信支付结果通知的回调地址，通知url必须为外网可访问的url，不能携带参数。
+      map.put("notify_url","");
+      map.put("trade_type","JSAPI");
+
+      return null;
+  }
 
 }
