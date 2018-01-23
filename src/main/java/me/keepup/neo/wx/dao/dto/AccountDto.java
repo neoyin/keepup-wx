514eb135d05aac126eb8d130954f45997585bb25
NeoYin <yinwei6@jd.com>
2018-01-23 18:32:31 +0800

    add mongodb and dto

---
+++ src/main/java/me/keepup/neo/wx/dao/dto/AccountDto.java
@@ -0,0 +1,66 @@
+package me.keepup.neo.wx.dao.dto;
+
+import me.keepup.neo.wx.bean.Weixin;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "user")
+public class AccountDto  {
+    @Id
+    private String id;
+    private Date create_time;
+    private Weixin weixin;
+    private String openid;
+    private String phone;
+    private int status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public Weixin getWeixin() {
+        return weixin;
+    }
+
+    public void setWeixin(Weixin weixin) {
+        this.weixin = weixin;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
