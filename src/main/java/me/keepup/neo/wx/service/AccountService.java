4dc3ae15b33a9c2ebe29b95821a27a032f127dc0
NeoYin <yinwei6@jd.com>
2018-01-24 11:21:11 +0800

    add account save

--- src/main/java/me/keepup/neo/wx/service/AccountService.java
+++ src/main/java/me/keepup/neo/wx/service/AccountService.java
@@ -1,7 +1,10 @@
 package me.keepup.neo.wx.service;
 
+import me.keepup.neo.wx.bean.Weixin;
+import me.keepup.neo.wx.dao.dto.AccountDto;
+
 public interface AccountService {
 
-    
+    AccountDto saveAccount(Weixin weixin);
 
 }
---
+++ src/main/java/me/keepup/neo/wx/service/impl/AccountServiceImpl.java
@@ -0,0 +1,24 @@
+package me.keepup.neo.wx.service.impl;
+
+import me.keepup.neo.wx.bean.Weixin;
+import me.keepup.neo.wx.dao.AccountDao;
+import me.keepup.neo.wx.dao.dto.AccountDto;
+import me.keepup.neo.wx.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+    @Override
+    public AccountDto saveAccount(Weixin weixin) {
+
+        AccountDto account = new AccountDto();
+        account.setCreate_time(new Date());
+        account.setOpenid(weixin.getOpenid());
+        return accountDao.save(account);
+    }
+}
